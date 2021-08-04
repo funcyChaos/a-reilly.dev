da6b6afe62f9ea7eddc127203b497193665d08e4
Austin Reilly <areilly@alphaworks.tech>
2021-08-04 10:53:58 -0700

    I added a lot Really I added too much, this was/is a prime example of not commiting/pushing enough and I need to work on it I had a bunch of problems and had to make backups and restore to find it, don't make  this many changes without a commit ever again. work on one thing and then push your changes currently nav bar is not working perfectly anymore, but most everything else is. The next commit should be blog preview related

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const navToggle = () =>
 {
 	const navBar = document.getElementById("nav-id");
 
-	if (navBar.className === "column nav-items")
+	if (navBar.className === "column-menu nav-items")
 	{
 			navBar.classList.add('hide')
 	}
